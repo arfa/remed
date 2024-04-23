cd066b3879eb3f193c085214663e1893b1fc7656
Marwen Arfa <marwen.arfa@gmail.com>
2024-04-23 03:47:36 +0100

    feat: add i18n support to the app

--- App.tsx
+++ App.tsx
@@ -5,12 +5,16 @@ import { name as appName } from './app.json';
 
 import App from './src';
 import { theme } from './src/core/theme';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './src/locales/i18n';
 
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
-      <App />
-    </PaperProvider>
+    <I18nextProvider i18n={i18n}>
+      <PaperProvider theme={theme}>
+        <App />
+      </PaperProvider>
+    </I18nextProvider>
   );
 }
 
---
+++ src/locales/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './en.json';
+import fr from './fr.json';
+
+const resources = {
+  en: en,
+  fr: fr,
+};
+
+i18n
+
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    compatibilityJSON: 'v3', //To make it work for Android devices, add this line.
+    resources,
+    lng: 'fr', // default language to use.
+    // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
+    // if you're using a language detector, do not define the lng option
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  });
+
+export default i18n;
